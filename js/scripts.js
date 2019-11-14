1b0b6327f72030fb9b48e567e0cca55af2ec919d
kasiadel <53861206+kasiadel@users.noreply.github.com>
2019-11-14 11:42:12 +0100

    Added squre brackets to type

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,7 @@
-var repository= [
-{name:"Ivysaur",weight:13,height:1,type:'grass','poison'},
-{name:"Charmander",weight:8.5,height:0.6,type:'fire'},
-{name:"Squirtle",weight:9,height:0.5,type:'water'},
-{name:"Caterpie",weight:2.9,height:0.3,type:'bug'},
-{name:"Raticate",weight:18.5,height:0.7,type:'normal'}
-];
-
-
-consol.log(repository[name]);
+  var repository= [
+  {name:"Ivysaur",weight:13,height:1,type:'grass',['poison']},
+  {name:"Charmander",weight:8.5,height:0.6,type:['fire']},
+  {name:"Squirtle",weight:9,height:0.5,type:['water']},
+  {name:"Caterpie",weight:2.9,height:0.3,type:['bug']},
+  {name:"Raticate",weight:18.5,height:0.7,type:['normal']}
+  ];
