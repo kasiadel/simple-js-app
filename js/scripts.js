d67441c666cebdefea3895bf19786941fb2395e1
kasiadel <53861206+kasiadel@users.noreply.github.com>
2019-12-12 13:55:50 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
 var pokemonRepository = function(){
-​
   var repository= [
     {
       name:"Ivysaur",
@@ -32,39 +31,56 @@ var pokemonRepository = function(){
       type:["normal"]
     }
   ];
-​
-​
-​
-​
-​
-    function addListItem(pokemon){
+
+    function addListItem(repository){
       var button = document.createElement("button");
       var li = document.createElement("li");
       button.innerText = repository.name;
       button.classList.add("my-button");
       li.appendChild(button);
       ul.appendChild(li);
-      };
-​
+      button.addEventListener("click",showDetails(repository.name));
+      }
+
+    function showDetails(repository){
+    console.log(repository);
+     }
       function add(pokemon) {
          repository.push(pokemon);
       }
        function getAll() {
          return repository;
        }
-​
+
     return {
         add: add,
         getAll: getAll,
        addListItem: addListItem
-​
+
 };
 }();
 //console.log(pokemonRepository.getAll());
 //pokemonRepository.add({ name: "Pidgeot" });
 var ul = document.querySelector(".pokemon-list");
-​
-​
+
 pokemonRepository.getAll().forEach(function(repository){
-  addListItem
+  pokemonRepository.addListItem(repository);
 });
+
+//var button = document.querySelectorAll("button");
+//for(var i=0; i< button.length; i++){
+//button[i].addEventListener("click",
+//function(repository){
+//console.log(repository.name);
+//});
+
+
+//==== CHANGE  OF POKEMON  NAME COLOR ONCE CLICKED===
+
+/*var button = document.querySelectorAll("button");
+for(var i=0; i< button.length; i++){
+button[i].addEventListener("click",
+function(){
+this.style.color = "green";
+});
+}*/
