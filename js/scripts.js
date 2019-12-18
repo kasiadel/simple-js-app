254f583f8bd094e160508769162f8fe88167d838
kasiadel <53861206+kasiadel@users.noreply.github.com>
2019-12-18 17:23:45 +0100

    changes in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,89 +1,88 @@
-var pokemonRepository = function(){
-  var repository= [
-    {
-      name:"Ivysaur",
-      weight:13,
-      height:1,
-      type:["grass","poison"]
-    },
-    {
-      name:"Charmander",
-      weight:8.5,
-      height:0.6,
-      type:["fire"]
-    },
-    {
-      name:"Squirtle",
-      weight:9,
-      height:0.5,
-      type:["water"]
-    },
-    {
-      name:"Caterpie",
-      weight:2.9,
-      height:0.3,
-      type:["bug"]
-    },
-    {
-      name:"Raticate",
-      weight:18.5,
-      height:0.7,
-      type:["normal"]
-    }
-  ];
+var pokemonRepository =(function(){
+  var repository= [];
+  var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
-    function addListItem(repository){
-      var button = document.createElement("button");
-      var li = document.createElement("li");
-      button.innerText = repository.name;
-      button.classList.add("my-button");
-      li.appendChild(button);
-      ul.appendChild(li);
-      button.addEventListener("click",function(event){
-        showDetails(repository.name);
-      });
+  function addListItem(repository){
+  var button = document.createElement("button");
+  var li = document.createElement("li");
+  button.innerText = repository.name;
+  button.classList.add("my-button");
+  li.appendChild(button);
+  ul.appendChild(li);
+  button.addEventListener("click",function(event){
+  showDetails(repository.name);
+    });
+  }
+
+  function showDetails(repository){
+  console.log(repository);
+    }
+  function add(pokemon) {
+  repository.push(pokemon);
+    }
+  function getAll() {
+  return repository;
     }
 
-    function showDetails(repository){
-    console.log(repository);
-     }
-      function add(pokemon) {
-         repository.push(pokemon);
-      }
-       function getAll() {
-         return repository;
-       }
+
+  function loadList() {
+      return fetch(apiUrl).then(function (response) {
+        return response.json();
+      }).then(function (json) {
+        json.results.forEach(function (item) {
+          var pokemon = {
+        name: item.name,
+        detailsUrl: item.url
+    };
+      add(pokemon);
+    });
+  }).catch(function (e) {
+    console.error(e);
+  });
+}
+
+  function loadDetails(item) {
+    var url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      // Now we add the details to the item
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = Object.keys(details.types);
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
+
+
 
     return {
-        add: add,
-        getAll: getAll,
-       addListItem: addListItem
+    add: add,
+    getAll: getAll,
+    search:search,
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails
+  };
+})();
 
-};
+pokemonRepository.loadList(pokemon).then(function() {
+  pokemonRepository.getAll().forEach(function(pokemon){
+   addListItem(pokemon);
+     });
+    });
+
+function showDetails(item) {
+pokemonRepository.loadDetails(item).then(function() {
+console.log(item);
+ });
 }
-();
-//console.log(pokemonRepository.getAll());
-//pokemonRepository.add({ name: "Pidgeot" });
+
+
+
 var ul = document.querySelector(".pokemon-list");
 
 pokemonRepository.getAll().forEach(function(repository){
-  pokemonRepository.addListItem(repository);
-});
-
-//var button = document.querySelectorAll("button");
-//for(var i=0; i< button.length; i++){
-//button[i].addEventListener("click",
-//function(repository){
-//console.log(repository.name);
-//});
-
-
-//==== CHANGE  OF POKEMON  NAME COLOR ONCE CLICKED===
-
-/*var button = document.querySelectorAll("button");
-for(var i=0; i< button.length; i++){
-button[i].addEventListener("click",
-function(){
-this.style.color = "green";
-});
-}*/
+pokemonRepository.addListItem(repository);
+ });
